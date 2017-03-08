76158c1833b42dbdfcf94b55ec6d4236e7b1a3bb
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-03-07 18:52:46 -0600

    adding bootstrap gem

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,8 @@ class ContactsController < ApplicationController
   def contact_one_method
     @contact = Contact.first
     @second_contact = Contact.second
-    @third_contact = Contact.third 
+    @third_contact = Contact.third
+    @contacts = Contact.all 
     render "contact_one.html.erb"
   end
 end
