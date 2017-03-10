a9428143b403b4fdb6e6018f03d2f21f8e9da4b2
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-03-09 19:09:14 -0600

    using params to add a new contact, routing the user to a page that thanks their first name

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,19 @@
 class ContactsController < ApplicationController
+  def all_contacts_method
+    @contacts = Contact.all
+    render "all_contacts.html.erb"
+  end
   def contact_one_method
-    @contact = Contact.first
-    @second_contact = Contact.second
-    @third_contact = Contact.third
-    @contacts = Contact.all 
-    render "contact_one.html.erb"
+    @contact = Contact.find_by(id: params["contact_id"])
+    render "one_contact.html.erb"
+  end
+  def new_contact_method
+    render "new_contact.html.erb"
+  end
+  def contact_added_method
+    @contact_first_name = params["first_name"]
+    render "contact_added_success.html.erb"
   end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  get "/firstcontact" => "contacts#contact_one_method"
+  get "/one_contact_url/:contact_id" => "contacts#contact_one_method"
+  get "/all_contacts_url" => "contacts#all_contacts_method"
+  get "/new_contact_url" => "contacts#new_contact_method"
+  post "/contact_added_url" => "contacts#contact_added_method"
+  
 end
